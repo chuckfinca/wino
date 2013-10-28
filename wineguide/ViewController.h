5d0f2ac3f7c002fb2bb2b0627343d61912dc6c33
Charles Feinn <chuckfinca@gmail.com>
2013-10-28 14:00:01 -0700

    Initial Commit

---
+++ wineguide/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  wineguide
+//
+//  Created by Charles Feinn on 10/28/13.
+//  Copyright (c) 2013 AppSimple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ wineguide/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  wineguide
+//
+//  Created by Charles Feinn on 10/28/13.
+//  Copyright (c) 2013 AppSimple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
