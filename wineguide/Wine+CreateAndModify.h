915328cb672a69abc0a83e03139a8fd8675fae6d
Charles Feinn <chuckfinca@gmail.com>
2013-10-31 14:15:18 -0700

    CATEGORY - added category to Wine class to handle creation and modification

---
+++ wineguide/Wine+CreateAndModify.h
@@ -0,0 +1,13 @@
+//
+//  Wine+CreateAndModify.h
+//  wineguide
+//
+//  Created by Charles Feinn on 10/31/13.
+//  Copyright (c) 2013 AppSimple. All rights reserved.
+//
+
+#import "Wine.h"
+
+@interface Wine (CreateAndModify)
+
+@end
---
+++ wineguide/Wine+CreateAndModify.m
@@ -0,0 +1,13 @@
+//
+//  Wine+CreateAndModify.m
+//  wineguide
+//
+//  Created by Charles Feinn on 10/31/13.
+//  Copyright (c) 2013 AppSimple. All rights reserved.
+//
+
+#import "Wine+CreateAndModify.h"
+
+@implementation Wine (CreateAndModify)
+
+@end
