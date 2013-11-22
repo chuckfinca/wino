fa2238f6575a55c71027dd838ba6d8844fc15207
Charles Feinn <chuckfinca@gmail.com>
2013-11-22 11:09:48 -0800

    Added new ColorSchemer class to control color scheme throughout the app

---
+++ wineguide/model/ColorSchemer.h
@@ -0,0 +1,13 @@
+//
+//  ColorSchemer.h
+//  wineguide
+//
+//  Created by Charles Feinn on 11/22/13.
+//  Copyright (c) 2013 AppSimple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ColorSchemer : NSObject
+
+@end
---
+++ wineguide/model/ColorSchemer.m
@@ -0,0 +1,13 @@
+//
+//  ColorSchemer.m
+//  wineguide
+//
+//  Created by Charles Feinn on 11/22/13.
+//  Copyright (c) 2013 AppSimple. All rights reserved.
+//
+
+#import "ColorSchemer.h"
+
+@implementation ColorSchemer
+
+@end
