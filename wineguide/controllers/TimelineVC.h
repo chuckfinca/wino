9a5206def27eaf32a2fd22f7cc0b38b5f02e5dd2
Charles Feinn <chuckfinca@gmail.com>
2013-12-11 15:06:51 -0800

    Added TimelineVC

---
+++ wineguide/controllers/TimelineVC.h
@@ -0,0 +1,13 @@
+//
+//  TimelineVC.h
+//  Gimme
+//
+//  Created by Charles Feinn on 12/10/13.
+//  Copyright (c) 2013 AppSimple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimelineVC : UIViewController
+
+@end
---
+++ wineguide/controllers/TimelineVC.m
@@ -0,0 +1,38 @@
+//
+//  TimelineVC.m
+//  Gimme
+//
+//  Created by Charles Feinn on 12/10/13.
+//  Copyright (c) 2013 AppSimple. All rights reserved.
+//
+
+#import "TimelineVC.h"
+
+@interface TimelineVC ()
+
+@end
+
+@implementation TimelineVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
