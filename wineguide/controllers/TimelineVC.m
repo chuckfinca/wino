88cbcffd344738ff8f9f79eb1e4c8cbc9718e505
Charles Feinn <chuckfinca@gmail.com>
2013-12-11 19:03:57 -0800

    Wired up new WineCardCell to display all wines, as a POC

--- wineguide/controllers/TimelineVC.m
+++ wineguide/controllers/TimelineVC.m
@@ -7,8 +7,19 @@
 //
 
 #import "TimelineVC.h"
+#import "WineCardCell.h"
+#import <CoreData/CoreData.h>
+#import "DocumentHandler.h"
+#import "Wine.h"
 
-@interface TimelineVC ()
+#define WINE_CARD_CELL @"WineCardCell"
+#define WINE_ENTITY @"Wine"
+
+@interface TimelineVC () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (nonatomic, strong) NSManagedObjectContext *context;
+@property (weak, nonatomic) IBOutlet UICollectionView *wineCardCollectionView;
+@property (nonatomic, strong) NSArray *wines;
 
 @end
 
@@ -27,12 +38,124 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    [self.wineCardCollectionView registerNib:[UINib nibWithNibName:WINE_CARD_CELL bundle:nil] forCellWithReuseIdentifier:WINE_CARD_CELL];
+    self.wineCardCollectionView.showsHorizontalScrollIndicator = NO;
+    self.wineCardCollectionView.backgroundColor = [UIColor whiteColor];
+    self.title = @"Timeline";
+    
+    [self refresh];
 }
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.wineCardCollectionView.collectionViewLayout invalidateLayout];
+}
+
+
+#pragma mark - Getters & Setters
+
+
+#pragma mark - Setup
+
+-(void)refresh
+{
+    [self getManagedObjectContext];
+    if (self.context){
+        [self setupFetchedResultsController];
+    }
+}
+
+-(void)getManagedObjectContext
+{
+    if(!self.context && [DocumentHandler sharedDocumentHandler]){
+        self.context = [DocumentHandler sharedDocumentHandler].document.managedObjectContext;
+    } else {
+        [self listenForDocumentReadyNotification];
+        NSLog(@"cannot get managed object context because either self.context exists (%@) or [DocumentHandler sharedDocumentHandler] does not exist (%@). Did start listening to DocumentReady notifications",self.context,[DocumentHandler sharedDocumentHandler]);
+    }
+}
+
+-(void)setupFetchedResultsController
+{
+    //NSLog(@"Timeline setupFetchedResultsController...");
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:WINE_ENTITY];
+    request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"identifier"
+                                                              ascending:YES
+                                                               selector:@selector(localizedCaseInsensitiveCompare:)]];
+    
+    request.predicate = nil;
+    
+    NSError *error;
+    self.wines = [self.context executeFetchRequest:request error:&error];
+    NSLog(@"num results = %i",[self.wines count]);
+}
+
+
+#pragma mark - UICollectionViewDataSource
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    NSLog(@"numberOfSectionsInCollectionView");
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    NSLog(@"numberOfItemsInSection");
+    NSInteger numItems = 0;
+    
+    if(collectionView == self.wineCardCollectionView){
+        NSLog(@"[self.wines count] = %i",[self.wines count]);
+        return [self.wines count];
+    }
+    
+    return numItems;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell;
+    NSLog(@"cellForItemAtIndexPath");
+    
+    if(collectionView == self.wineCardCollectionView){
+        WineCardCell *wineCardCell = [collectionView dequeueReusableCellWithReuseIdentifier:WINE_CARD_CELL forIndexPath:indexPath];
+        
+        Wine *wine = [self.wines objectAtIndex:indexPath.row];
+        [wineCardCell setupCardWithWine:wine];
+        
+        cell = wineCardCell;
+    }
+    
+    return cell;
+}
+
+
+
+
+#pragma mark - Listen for Notifications
+
+-(void)listenForDocumentReadyNotification
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(refresh)
+                                                 name:@"Document Ready"
+                                               object:nil];
+}
+
+
+
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
