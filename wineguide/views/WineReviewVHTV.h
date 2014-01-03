4555bf7235f343ace196154c543193abdda66dc5
Charles Feinn <chuckfinca@gmail.com>
2014-01-03 15:43:56 -0800

    skeleton of forth coming WineReviewVHTV class

---
+++ wineguide/views/WineReviewVHTV.h
@@ -0,0 +1,13 @@
+//
+//  WineReviewVHTV.h
+//  Corkie
+//
+//  Created by Charles Feinn on 1/3/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import "VariableHeightTV.h"
+
+@interface WineReviewVHTV : VariableHeightTV
+
+@end
---
+++ wineguide/views/WineReviewVHTV.m
@@ -0,0 +1,31 @@
+//
+//  WineReviewVHTV.m
+//  Corkie
+//
+//  Created by Charles Feinn on 1/3/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import "WineReviewVHTV.h"
+
+@implementation WineReviewVHTV
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
