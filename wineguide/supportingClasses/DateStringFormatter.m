eee90afe395f8633d2ea02bd91c351f969b48710
Charles Feinn <chuckfinca@gmail.com>
2014-04-30 14:06:47 -0700

    REFACTOR - DateStringFormatter for dealing with tasting dates. Now it only pays attention to the users local day and doesn't use seconds to determine what to show.

--- wineguide/supportingClasses/DateStringFormatter.m
+++ wineguide/supportingClasses/DateStringFormatter.m
@@ -10,50 +10,43 @@
 #import "ColorSchemer.h"
 #import "FontThemer.h"
 
-#define SECONDS_IN_A_WEEK 604800
-#define SECONDS_IN_A_DAY 86400
-#define SECONDS_IN_30_MINUTES 1800
-
 @implementation DateStringFormatter
 
 +(NSString *)formatStringForTimelineDate:(NSDate *)date
 {
-    NSTimeInterval timeSinceTasting = [[NSDate date] timeIntervalSinceDate:date];
-    
     NSString *localDateString;
     
-    if(timeSinceTasting > SECONDS_IN_A_WEEK){
-        NSDateFormatter *dateFormatter = [NSDateFormatter new];
-        dateFormatter.dateFormat = @"MM/dd/YY";
-        localDateString = [dateFormatter stringFromDate:date];
-        
-        NSRange dayZero = NSMakeRange(3, 1);
-        if([[localDateString substringWithRange:dayZero] isEqualToString:@"0"]){
-            localDateString = [localDateString stringByReplacingCharactersInRange:dayZero withString:@""];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *tastingDateComponents = [calendar components:(NSDayCalendarUnit | NSMonthCalendarUnit) fromDate:date];
+    NSDateComponents *currentDateComponents = [calendar components:(NSDayCalendarUnit | NSMonthCalendarUnit) fromDate:[NSDate date]];
+    
+    if(tastingDateComponents.year == currentDateComponents.year && tastingDateComponents.month == currentDateComponents.month){
+        if (tastingDateComponents.day == currentDateComponents.day) {
+            localDateString = @"today";
+        } else if (currentDateComponents.day - tastingDateComponents.day < 7){
+            localDateString = [NSString stringWithFormat:@"%ldd",(long)(currentDateComponents.day - tastingDateComponents.day)];
+        } else {
+            localDateString = [DateStringFormatter defaultFormattedTastingRecordDateStringForRawDate:date WithYear:tastingDateComponents.year andCurrentYear:currentDateComponents.year];
         }
-        if([[localDateString substringToIndex:1] isEqualToString:@"0"]){
-            localDateString = [localDateString substringFromIndex:1];
-        }
-        
-    } else if(timeSinceTasting > SECONDS_IN_A_DAY){
-        int daysSinceTasting = timeSinceTasting/86400;
-        localDateString = [NSString stringWithFormat:@"%@d",@(daysSinceTasting)];
-        
-    } else if(timeSinceTasting > SECONDS_IN_30_MINUTES){
-        int hoursSinceTasting = timeSinceTasting/3600;
-        if(hoursSinceTasting == 0){
-            hoursSinceTasting++;
-        }
-        localDateString = [NSString stringWithFormat:@"%@h",@(hoursSinceTasting)];
-        
-    } else  if(timeSinceTasting > 60){
-        int minutesSinceTasting = timeSinceTasting/60;
-        localDateString = [NSString stringWithFormat:@"%@m",@(minutesSinceTasting)];
-        
     } else {
-        localDateString = @"just now";
+        localDateString = [DateStringFormatter defaultFormattedTastingRecordDateStringForRawDate:date WithYear:tastingDateComponents.year andCurrentYear:currentDateComponents.year];
     }
+    
+    return localDateString;
+}
 
++(NSString *)defaultFormattedTastingRecordDateStringForRawDate:(NSDate *)date WithYear:(NSInteger)tastingDateYear andCurrentYear:(NSInteger)currentYear
+{
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    dateFormatter.dateFormat = @"MM/dd/YY";
+    
+    NSString *localDateString = [dateFormatter stringFromDate:date];
+    
+    if(tastingDateYear == currentYear){
+        localDateString = [localDateString substringToIndex:4];
+    }
+    localDateString = [localDateString stringByReplacingOccurrencesOfString:@"0" withString:@""];
+    
     return localDateString;
 }
 
