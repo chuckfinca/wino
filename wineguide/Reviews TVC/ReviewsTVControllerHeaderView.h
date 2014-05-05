797302cfdd35bce469d5a9a58e4041caf3913ebb
Charles Feinn <chuckfinca@gmail.com>
2014-05-04 20:21:49 -0700

    FIX - brought back ReviewsTVControllerHeaderView which was accidentally deleted

---
+++ wineguide/Reviews TVC/ReviewsTVControllerHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  ReviewsTVControllerHeaderView.h
+//  Corkie
+//
+//  Created by Charles Feinn on 3/31/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReviewsTVControllerHeaderView : UIView
+
+@end
