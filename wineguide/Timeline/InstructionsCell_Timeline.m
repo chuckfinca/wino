1a8989e78b178fec290073483977894e46981325
Charles Feinn <chuckfinca@gmail.com>
2014-05-08 16:48:26 -0700

    Create's Timeline instructions cell and adds images

---
+++ wineguide/Timeline/InstructionsCell_Timeline.m
@@ -0,0 +1,25 @@
+//
+//  InstructionsCell_Timeline.m
+//  Corkie
+//
+//  Created by Charles Feinn on 5/8/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import "InstructionsCell_Timeline.h"
+
+@implementation InstructionsCell_Timeline
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
