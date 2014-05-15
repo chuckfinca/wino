ef2cc226cbf1d61f8bfe591e82c604856d50a502
Charles Feinn <chuckfinca@gmail.com>
2014-05-15 14:27:07 -0700

    Adds ReviewsLabel class

---
+++ wineguide/views/ReviewsLabel.m
@@ -0,0 +1,31 @@
+//
+//  ReviewsLabel.m
+//  Corkie
+//
+//  Created by Charles Feinn on 5/15/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import "ReviewsLabel.h"
+
+@implementation ReviewsLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
