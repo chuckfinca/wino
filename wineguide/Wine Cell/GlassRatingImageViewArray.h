048be90ee08d0b8c0982d8eb2116b2822f28f9eb
Charles Feinn <chuckfinca@gmail.com>
2014-05-15 14:03:51 -0700

    Creates GlassRatingImageView class

---
+++ wineguide/Wine Cell/GlassRatingImageViewArray.h
@@ -0,0 +1,13 @@
+//
+//  GlassRatingImageViewArray.h
+//  Corkie
+//
+//  Created by Charles Feinn on 5/15/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GlassRatingImageViewArray : NSArray
+
+@end
---
+++ wineguide/Wine Cell/GlassRatingImageViewArray.m
@@ -0,0 +1,13 @@
+//
+//  GlassRatingImageViewArray.m
+//  Corkie
+//
+//  Created by Charles Feinn on 5/15/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import "GlassRatingImageViewArray.h"
+
+@implementation GlassRatingImageViewArray
+
+@end
