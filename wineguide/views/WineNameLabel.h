5c67e89c1d266a07f86e3981aead9b91b013f957
Charles Feinn <chuckfinca@gmail.com>
2014-05-15 15:50:38 -0700

    creates WineNameLabel

---
+++ wineguide/views/WineNameLabel.h
@@ -0,0 +1,13 @@
+//
+//  WineNameLabel.h
+//  Corkie
+//
+//  Created by Charles Feinn on 5/15/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WineNameLabel : UILabel
+
+@end
---
+++ wineguide/views/WineNameLabel.m
@@ -0,0 +1,31 @@
+//
+//  WineNameLabel.m
+//  Corkie
+//
+//  Created by Charles Feinn on 5/15/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import "WineNameLabel.h"
+
+@implementation WineNameLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
