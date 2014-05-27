eeb6862ee985c40b417ad29837a9fbd1ce6cec25
Charles Feinn <chuckfinca@gmail.com>
2014-05-27 15:24:31 -0700

    Added instructions arrow image and wired up cell

--- wineguide/Restaurant/InstructionsCell_TriedIt.m
+++ wineguide/Restaurant/InstructionsCell_TriedIt.m
@@ -7,12 +7,27 @@
 //
 
 #import "InstructionsCell_TriedIt.h"
+#import "ColorSchemer.h"
+#import "FontThemer.h"
+
+@interface InstructionsCell_TriedIt ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *arrowImageView;
+@property (weak, nonatomic) IBOutlet UILabel *instructionsLabel;
+
+@end
 
 @implementation InstructionsCell_TriedIt
 
 - (void)awakeFromNib
 {
     // Initialization code
+    [self.arrowImageView setImage:[[UIImage imageNamed:@"instructions_arrow_up.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
+    self.arrowImageView.tintColor = [ColorSchemer sharedInstance].baseColor;
+    
+    self.instructionsLabel.attributedText = [[NSAttributedString alloc] initWithString:@"Click 'Tried It' above to check this wine into your Timeline. Be sure to tag your friends to see what they thought!" attributes:[FontThemer sharedInstance].secondaryBodyTextAttributes];
+    
+    self.backgroundColor = [ColorSchemer sharedInstance].customBackgroundColor;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
