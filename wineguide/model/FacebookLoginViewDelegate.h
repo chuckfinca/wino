c40139150d54b10030939f87a677a97f7a1c3241
Charles Feinn <chuckfinca@gmail.com>
2014-05-29 19:02:25 -0700

    Created FacebookLoginViewDelegate to field the various login views around the app. each reports back to its delegate, FacebookSessionManager

---
+++ wineguide/model/FacebookLoginViewDelegate.h
@@ -0,0 +1,13 @@
+//
+//  FacebookLoginViewDelegate.h
+//  Corkie
+//
+//  Created by Charles Feinn on 5/29/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FacebookLoginViewDelegate : NSObject
+
+@end
---
+++ wineguide/model/FacebookLoginViewDelegate.m
@@ -0,0 +1,13 @@
+//
+//  FacebookLoginViewDelegate.m
+//  Corkie
+//
+//  Created by Charles Feinn on 5/29/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import "FacebookLoginViewDelegate.h"
+
+@implementation FacebookLoginViewDelegate
+
+@end
