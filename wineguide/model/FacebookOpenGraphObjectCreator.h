5b1dabeceaa3cbb00db2159520c3e73d4c73a511
Charles Feinn <chuckfinca@gmail.com>
2014-06-17 16:43:56 -0700

    created new FacebookOpenGraphObjectCreator class to post tasting reviews to facebook

---
+++ wineguide/model/FacebookOpenGraphObjectCreator.h
@@ -0,0 +1,13 @@
+//
+//  FacebookOpenGraphObjectCreator.h
+//  Corkie
+//
+//  Created by Charles Feinn on 6/13/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FacebookOpenGraphObjectCreator : NSObject
+
+@end
---
+++ wineguide/model/FacebookOpenGraphObjectCreator.m
@@ -0,0 +1,13 @@
+//
+//  FacebookOpenGraphObjectCreator.m
+//  Corkie
+//
+//  Created by Charles Feinn on 6/13/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import "FacebookOpenGraphObjectCreator.h"
+
+@implementation FacebookOpenGraphObjectCreator
+
+@end
